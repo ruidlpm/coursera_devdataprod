7ba576f12d9fd745ca0c636738d72952b08a92d2
Rui Martiniano <ruidlpm@gmail.com>
2015-11-15 20:21:27 +0000

    Update server.R

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,1 +1,16 @@
+shinyServer(function(input, output) {
+  output$main_plot <- renderPlot({
+  require(visreg)
+  
+  do_rnorm <-function(x){
+	rnorm(x)
+  }
 
+  a<-do_rnorm(input$adjust)
+  b<-do_rnorm(input$adjust)
+
+  fit<-lm(a~b)
+  visreg(fit, xlab="var b", ylab="var a")
+
+  })
+})
