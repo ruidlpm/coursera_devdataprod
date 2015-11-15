1cb30ca65b133010b884cc2365c88f24eb6cf35c
Rui Martiniano <ruidlpm@gmail.com>
2015-11-15 20:21:53 +0000

    Update ui.R

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,1 +1,10 @@
+shinyUI(bootstrapPage(
 
+  plotOutput(outputId = "main_plot"),
+
+	sliderInput(inputId = "bw_adjust",
+        label = "Bandwidth adjustment:",
+        min = 10, max = 1000, value = 10, step = 10)
+  )
+
+)
