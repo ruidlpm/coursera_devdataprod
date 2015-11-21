68b89fa5b34ce8c9273a7b495f5df826c7e82d35
Rui Martiniano <ruidlpm@gmail.com>
2015-11-21 21:15:50 +0000

    added title and descr

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,10 +1,13 @@
 
 shinyUI(bootstrapPage(
 
+  # Application title
+  titlePanel("Mortality rate/100, under-5 (per 1,000 live births)"),
+  p("Use the slider to change the Year"),
   plotOutput(outputId = "main_plot"),
 
 	sliderInput(inputId = "Year",
-        label = "Adjustment:",
+        label = "Year:",
         min = 1965, max = 2014, value = 1965, step = 1)
   )
 
