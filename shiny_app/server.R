608ba5c3a7501e9cb935af5ce8209c44f97959c6
Rui Martiniano <ruidlpm@gmail.com>
2015-11-21 21:15:30 +0000

    corrected colour

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -2,15 +2,18 @@ shinyServer(function(input, output) {
   output$main_plot <- renderPlot({
 	require(maps)
 	require(plotrix)
-	df<-read.table("data/mortality.txt", h=T)
+	df<-read.table("mortality.txt", h=T)
 
-	test_col<-color.scale(df$X1965, extremes=c("black","blue", "yellow" ,"red"))
+	test_col<-color.scale(df[colnames(df)==paste("X",input$Year, sep="")][1:length(df),], extremes=c("black","blue", "yellow" ,"red"))
 
 	map(database="world", fill=T, plot=T,interior=F, col="navajowhite3", border = "navajowhite3", bg="lightblue")
 	map("world", interior = F, add = T)
 	points(df$lon, df$lat, bg=test_col, cex=round(df[colnames(df)==paste("X",input$Year, sep="")][1:length(df),]/10)/10, pch=21)
-	mtext("Mortality rate/100, under-5 (per 1,000 live births)")
+
 
 
   })
 })
+
+
+
