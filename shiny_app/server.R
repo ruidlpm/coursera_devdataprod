4fb6c5704e588f910f08bf21f5bfa94145ea6275
Rui Martiniano <ruidlpm@gmail.com>
2015-11-21 19:11:29 +0000

    changed project

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,16 +1,16 @@
 shinyServer(function(input, output) {
   output$main_plot <- renderPlot({
-  require(visreg)
-  
-  do_rnorm <-function(x){
-	rnorm(x)
-  }
+	require(maps)
+	require(plotrix)
+	df<-read.table("data/mortality.txt", h=T)
 
-  a<-do_rnorm(input$adjust)
-  b<-do_rnorm(input$adjust)
+	test_col<-color.scale(df$X1965, extremes=c("black","blue", "yellow" ,"red"))
 
-  fit<-lm(a~b)
-  visreg(fit, xlab="var b", ylab="var a")
+	map(database="world", fill=T, plot=T,interior=F, col="navajowhite3", border = "navajowhite3", bg="lightblue")
+	map("world", interior = F, add = T)
+	points(df$lon, df$lat, bg=test_col, cex=round(df[colnames(df)==paste("X",input$Year, sep="")][1:length(df),]/10)/10, pch=21)
+	mtext("Mortality rate/100, under-5 (per 1,000 live births)")
+
 
   })
 })
